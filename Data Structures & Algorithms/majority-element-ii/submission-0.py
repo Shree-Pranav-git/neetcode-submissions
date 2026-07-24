c56c69b407360b930dc98d7302c57f0590ba7b39
Shree-Pranav-git <tinkuspp142006@gmail.com>
2026-07-24 15:03:14 +0530

    Add: majority-element-ii - submission-0

---
+++ Data Structures & Algorithms/majority-element-ii/submission-0.py
@@ -0,0 +1,15 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        d=dict()
+        res=[]
+        for i in nums:
+            if i not in d:
+                d[i]=1
+            else:
+                d[i]+=1
+        for i in d:
+            if d[i]>len(nums)//3:
+                res.append(i)
+        return res
+
+        
